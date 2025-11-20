6feca8733acdf18b9aa41c76f87a2cafb249eac2
Realsingh123 <160656036+Realsingh123@users.noreply.github.com>
2025-11-20 23:47:02 +0530

    updated

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,3 +1,28 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: "dist",
+//     sourcemap: false,           // disable sourcemaps for smaller build
+//     minify: "esbuild",          // fastest minifier (default)
+//     cssCodeSplit: true,         // better performance for large apps
+//     reportCompressedSize: true, // show gzip/brotli sizes
+//     chunkSizeWarningLimit: 800, // prevent unnecessary warnings
+//     rollupOptions: {
+//       output: {
+//         manualChunks(id) {
+//           // splits big dependencies automatically
+//           if (id.includes("node_modules")) {
+//             return id.toString().split("node_modules/")[1].split("/")[0];
+//           }
+//         },
+//       },
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -5,20 +30,5 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
-    sourcemap: false,           // disable sourcemaps for smaller build
-    minify: "esbuild",          // fastest minifier (default)
-    cssCodeSplit: true,         // better performance for large apps
-    reportCompressedSize: true, // show gzip/brotli sizes
-    chunkSizeWarningLimit: 800, // prevent unnecessary warnings
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // splits big dependencies automatically
-          if (id.includes("node_modules")) {
-            return id.toString().split("node_modules/")[1].split("/")[0];
-          }
-        },
-      },
-    },
   },
 });
